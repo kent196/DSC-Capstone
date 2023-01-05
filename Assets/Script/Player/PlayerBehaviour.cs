1767b93c15a0b31c4cfd9d7a1a64614c24af59e2
cris nguyen <kingericvt96@gmail.com>
2023-01-05 22:20:14 +0700

    add flower attack and remove start scene

--- Assets/Script/Player/PlayerBehaviour.cs
+++ Assets/Script/Player/PlayerBehaviour.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+            Debug.Log(GameManager.gameManager.playerHealth.Health);
+        }
+    }
     private void TakeDamage(int dmgAmount)
     {
         //some conditon
