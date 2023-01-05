9ac602f8b5e232a645e42fecfcb9401b7137994f
Kent <kentdev196@gmail.com>
2023-01-05 20:55:36 +0700

    delete SuperTiled in Duong's local

--- Assets/SuperTiled2Unity/Import/Overworld'sTileSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Overworld'sTileSet" tilewidth="16" tileheight="16" tilecount="352" columns="22">
- <image source="DARK Edition Tileset with background Bao.png" width="352" height="256"/>
-</tileset>
