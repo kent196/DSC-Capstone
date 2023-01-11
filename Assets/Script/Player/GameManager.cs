3e053b52baa101e864787dce7c20a6f39af4aa9c
cris nguyen <kingericvt96@gmail.com>
2023-01-11 13:51:40 +0700

    elevator system

--- Assets/Script/Player/GameManager.cs
+++ Assets/Script/Player/GameManager.cs
@@ -1,21 +1,127 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager gameManager { get; private set; }
-
     public HealthStats playerHealth = new HealthStats(100, 100);
+    public static bool GameIsPaused = false;
+    public static bool GameHasEnded = false;
+    [SerializeField] public GameObject pauseMenu;
+    [SerializeField] public GameObject confirmBox;
+    [SerializeField] public GameObject gui;
+    [SerializeField] private GameObject endMenu;
+
     private void Awake()
     {
-        if(gameManager != null && gameManager != this)
+        if (gameManager != null && gameManager != this)
         {
             Destroy(this);
         }
         else
         {
             gameManager = this;
+        }
+    }
+
+
+
+
+    private void Start()
+    {
+        pauseMenu = FindObjectOfType<Canvas>().transform.Find("PauseMenu").gameObject;
+        pauseMenu.SetActive(false);
+        confirmBox = FindObjectOfType<Canvas>().transform.Find("ConfirmBox").gameObject;
+        confirmBox.SetActive(false);
+        gui = FindObjectOfType<Canvas>().transform.Find("GUI").gameObject;
+        endMenu = FindObjectOfType<Canvas>().transform.Find("EndMenu").gameObject;
+        endMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePauseGame();
+
+    }
+
+    public void HandlePauseGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        gui.SetActive(true);
+        Debug.Log("Resume");
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        gui.SetActive(false);
+        Debug.Log("Pause");
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void ExitToMenu()
+    {
+        Debug.Log("Confirm");
+        confirmBox.SetActive(true);
+    }
+
+    public void ConfirmYes()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ConfirmNo()
+    {
+        confirmBox.SetActive(false);
+    }
+
+    public void EndGame()
+    {
+        if (GameHasEnded == false)
+        {
+            GameHasEnded = true;
+            Debug.Log("Game Over");
+            endMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void Restart()
+    {
+        GameHasEnded = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Menu()
+    {
+        {
+
+            GameHasEnded = false;
+            SceneManager.LoadScene("Start");
         }
     }
 }
