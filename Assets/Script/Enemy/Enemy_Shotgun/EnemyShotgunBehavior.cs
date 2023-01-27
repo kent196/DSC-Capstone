043aa9b06fb0bffe504a61f18e0fafd361433b62
Kent <kentdev196@gmail.com>
2023-01-27 09:35:21 +0700

    add enemy sniper health, add anim hit and dead

---
+++ Assets/Script/Enemy/Enemy_Shotgun/EnemyShotgunBehavior.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotgunBehavior : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth = 300;
+    private Animator animator;
+    private bool isDead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsDead();
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        animator.SetBool("isHit", true);
+        health -= dmg;
+        animator.Play("Hit");
+        animator.SetBool("isHit", false);
+
+
+    }
+
+    public void IsDead()
+    {
+        if (health <= 0)
+        {
+            animator.Play("Dead");
+            isDead = true;
+            Destroy(gameObject, .5f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fireball"))
+        {
+            TakeDamage(50);
+        }
+    }
+}
